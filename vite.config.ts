4a3bd0b7ad0315a10cde9090e269141c3ba99633
sho kawamoto <k.sho626626@gmail.com>
2025-08-29 20:22:54 +0900

    Restructure to separate app and landing page

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    rollupOptions: {
+      input: {
+        app: './app.html',
+      },
+    },
+  },
 });
